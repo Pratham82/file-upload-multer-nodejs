6a1acf4d8c07a99799ce2a08500b161acf2ed837
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-03-12 19:55:23 +0530

    added code for uploading zip and extracting it on server

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const multer = require('multer')
+const Unzipper = require('decompress-zip')
 const app = express()
 const path = require('path')
+const fs = require('fs')
+const shelljs = require('shelljs')
 
 // Creating file storage engine
 const fileStorageEngine = multer.diskStorage({
@@ -10,7 +13,7 @@ const fileStorageEngine = multer.diskStorage({
   },
 
   filename: (req, file, cb) => {
-    cb(null, Date.now() + '__' + file.originalname)
+    cb(null, file.originalname)
   },
 })
 
@@ -21,6 +24,38 @@ const upload = multer({ storage: fileStorageEngine })
 app.post('/single', upload.single('singleFile'), (req, res) => {
   const fileData = req.file
   res.json({ status: 'Single File sussessfully uploaded', data: fileData })
+})
+
+// Upload zipped files
+app.post('/unzip', upload.single('singleFile'), (req, res) => {
+  try {
+    if (req.file) {
+      //const fileData = req.file
+      const filePath = path.join(req.file.destination, req.file.filename)
+      const unzipper = new Unzipper(filePath)
+      // console.log(filePath)
+
+      // Unzipping
+      unzipper.on('extract', () => console.log('Finished extracting'))
+
+      // Unzip
+      unzipper.extract({
+        path: `/home/pratham82/Dev/file-upload-multer-nodejs/storage`,
+      })
+
+      // Deleting Zip file
+      // fs.unlinkSync(filePath, () => console.log('File Scuccessfully Deleted.'))
+    }
+    res.json({
+      status: 'Single File sussessfully uploaded, and file unzipped',
+      // data: fileData,
+    })
+  } catch (e) {
+    /* handle error */
+    res.status(400).json({
+      message: 'Errror in file upload',
+    })
+  }
 })
 
 //Upload multiple Files
@@ -34,6 +69,11 @@ app.get('/api', (_, res) => res.send('File upload API'))
 
 // FrontEnd route
 app.get('/client', (_, res) => res.sendFile(path.join(__dirname, 'index.html')))
+
+// Zipped route
+app.get('/zipped', (_, res) =>
+  res.sendFile(path.join(__dirname, 'zipped.html'))
+)
 
 app.listen(process.env.PORT || 3000, () =>
   console.log(`Server is running on PORT: ${process.env.PORT || 3000}`)
